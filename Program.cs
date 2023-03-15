c1af7c3d76b2a5d0e16b9420bc590853260381bf
ERICK GABRIEL MAJANO LOPEZ <egmajano@correo.url.edu.gt>
2023-03-14 19:36:55 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Newtonsoft.Json;
+namespace lab2
+{
+    public class Apartment
+    {
+        public bool isPetFriendly { get; set; }
+        public string address { get; set; }
+        public double price { get; set; }
+        public string contactPhone { get; set; }
+        public string id { get; set; }
+    }
+    public class House
+    {
+        public string zoneDangerous { get; set; }
+        public string address { get; set; }
+        public double price { get; set; }
+        public string contactPhone { get; set; }
+        public string id { get; set; }
+    }
+    public class Premise
+    {
+        public string[] commercialActivities { get; set; }
+        public string address { get; set; }
+        public double price { get; set; }
+        public string contactPhone { get; set; }
+        public string id { get; set; }
+    }
+    public class Builds
+    {
+        public Premise[] Premises { get; set; }
+        public Apartment[] Apartments { get; set; }
+        public House[] Houses { get; set; }
+    }
+    public class Input1
+    {
+        public Dictionary<string, bool> services { get; set; }
+        public Builds builds { get; set; }
+
+    }
+    public class Input2
+    {
+        public double budget { get; set; }
+        public string typeBuilder { get; set; }
+        public string[] requiredServices { get; set; }
+        public string commercialActivity { get; set; }
+        public bool? wannaPetFriendly { get; set; }
+        public string minDanger { get; set; }
+    }
+    public class InputLab
+    {
+        public Input1[] input1 { get; set; }
+        public Input2 input2 { get; set; }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+        }
+    }
+}
